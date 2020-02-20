27e8a0f30da3f712c84d25ae76c0594b56f25c0a
Dominique Pavlin <dpavlin@i-ozo.com>
2020-02-20 11:59:29 +0100

    variable

--- consoleTest/main.cpp
+++ consoleTest/main.cpp
@@ -1,11 +1,42 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+/**
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qInfo() << "Hello World";
-    qInfo("Hello again");
+
+    // Variables
+    bool isOn = true;
+    qInfo() << "is it on" << isOn;
+
+    int age = 53;
+    double height = 1.84;
+
+    qInfo() << "Age"<<age;
+    qInfo() << "Height"<<height;
+
+    signed int dogs=-1;
+    dogs = 15;
+
+
+    const unsigned int cats = 4;
+
+    qInfo() << "Dogs" << dogs;
+    qInfo() << "Cats" << cats;
+
+    enum Color{red=100,green=43,blue=5678};
+
+    Color myColor = Color::blue;
+
+    qInfo() << "My color"<<myColor;
+
 
     return a.exec();
 }
