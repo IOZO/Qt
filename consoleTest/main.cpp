8e13cc75dbce5c9f37158223e9cf63d40e1c2758
Dominique Pavlin <dpavlin@i-ozo.com>
2020-02-24 09:39:22 +0100

    overloading

--- consoleTest/main.cpp
+++ consoleTest/main.cpp
@@ -5,29 +5,20 @@
 using namespace std;
 
 
+void test(int age=0){
+    qInfo() << "age is"<<age;
+}
+
+void test(bool isActive=false){
+    (isActive)?qInfo()<<"Active":qInfo()<<"Not Active";
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int age = 0;
+    int age = 12;
 
-    qInfo() << "Enter your age:";
-    cin >> age;
-
-    if(age==0) qFatal("you did not enter a valid age !!!");
-
-    qInfo() << "You entered" << age;
-
-    if(age < 18){
-        qInfo()<<"You are under age!";
-        qInfo()<<"Please come back when you are old enough!";
-    }
-
-    if(age < 21 && age >= 18)
-        qInfo()<<"You are not really an adult yet!";
-
-    if(age >= 21) qInfo() << "You are adult !";
-
-    if(age > 150) qCritical() << "really ? are you human ? if yes, where did you find the fountain of jouvence !";
-
+    test(age);
+    test(true);
     return a.exec();
 }
