43be6d55550181c016d9213dfb654fdfe90ffd9e
Dominique Pavlin <dpavlin@i-ozo.com>
2020-02-26 16:25:09 +0100

    qobject_cast

--- consoleTest/main.cpp
+++ consoleTest/main.cpp
@@ -24,24 +24,12 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     racecar* player1 = new racecar(&a);
-    testDrive(player1); // implicit convertion. Old fashinon, can work but bad !!!
 
+    car* player2 = qobject_cast<car *>(player1);
+    player2->drive();
 
-    car *obj = dynamic_cast<car *>(player1);
-    if(obj) testDrive(obj); // dynamycally converted object with accurate controls is the way to go.
-
-
-    car* mycar = new car(&a);
-    racecar *fastcar = static_cast<racecar*>(mycar);
-    if(fastcar) race(fastcar); // upcasting the right way
-
-
-    int *pointer = reinterpret_cast<int*>(player1);
-    *pointer = 100;
-    qInfo() << "pointer = " << pointer << "value :"<<*pointer;
-
-    racecar* mysportcar = reinterpret_cast<racecar*>(pointer);
-    qInfo() << "my car color is " << mysportcar->color;
+    racecar* player3 = qobject_cast<racecar *>(player2);
+    player3->gofast();
 
     return a.exec();
 }
